668c3df8f27d15db5c37fd147b257924611e8ffe
SEERAT-ARSHAD <129464452+SEERAT-ARSHAD@users.noreply.github.com>
2023-12-26 17:13:38 +0500

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+// router.get('/haider', (req, res) =>{
+//     res.render('haider')
+// })
+
+
+router.get('/cities', (req, res) =>{
+    res.render('cities')
+})
+
+
+router.get('/contact', (req, res) =>{
+    res.render('contact')           
+})
+
+router.get('/personalities', (req, res) =>{
+    res.render('personalities')
+})
+
+
+router.get('/tspots', (req, res) =>{
+    res.render('tspots')
+})
